4804aaa81a771d0262936bfcd35aba7a66ccb6a6
JDBgit9 <info@jamesdbeeks.com>
2020-09-13 20:22:48 -0400

    new Element

---
+++ script.js
@@ -0,0 +1,25 @@
+var myEl = document.createElement("p");
+myEl.textContent = "What is 2 + 2?";
+document.body.append(myEl);
+
+var tagName = prompt("Please enter a valid response")
+
+
+
+
+
+
+var questions = [
+    {
+      title: "Example Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Example Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    }
+  ];
+  
+     
