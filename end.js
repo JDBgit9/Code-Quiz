e683758072641613e744ddc1ce57a759278d934f
JDBgit9 <info@jamesdbeeks.com>
2020-11-11 00:37:10 -0500

    added the timer and score displays

--- end.js
+++ end.js
@@ -6,7 +6,7 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 console.log(highScores);
 
-const MAX_HIGH_SCORES = 5;
+const MAX_HIGH_SCORES = 130;
 
 finalScore.innerText = mostRecentScore;
 
@@ -24,8 +24,8 @@ saveHighScore = e => {
   };
   highScores.push(score);
   highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
+  highScores.splice(130);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("./index.html");
+  window.location.assign("/public/index.html");
 };
